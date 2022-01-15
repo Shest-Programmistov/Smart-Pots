ab0f39c7632f0e13ac5cee8a5bdec75e151e1afb
Raluca <tudorr30@gmail.com>
2022-01-16 01:29:46 +0200

    Add watering event basic logic

--- water.py
+++ water.py
@@ -1,1 +1,38 @@
-# Un subscribe mqtt care ii spune pompei de apa cu cat sa ude la fiecare ?ora? ????
+"""Water logic"""
+from db import get_db
+
+def get_status():
+    # temperature = get_db().execute(
+    #     'SELECT id, timestamp, value'
+    #     ' FROM temperature'
+    #     ' ORDER BY timestamp DESC'
+    # ).fetchone()
+    temperature = get_db().execute(
+        'SELECT id, value'
+        ' FROM temperature'
+        ' ORDER BY id DESC'
+    ).fetchone()
+
+    # humidity = get_db().execute(
+    #     'SELECT id, timestamp, value'
+    #     ' FROM humidity'
+    #     ' ORDER BY timestamp DESC'
+    # ).fetchone()
+    humidity = get_db().execute(
+        'SELECT id, value'
+        ' FROM humidity'
+        ' ORDER BY id DESC'
+    ).fetchone()
+
+
+    if temperature is None:
+        return {'status': 'Please set a value for temperature'}
+
+    if humidity is None:
+        return {'status': 'Please set a value for humidity'}
+
+    return {
+        'data': {
+            'watered': "merge:)"
+        }
+    }
