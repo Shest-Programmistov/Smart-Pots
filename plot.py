1219c77dac20db8302dd02f99bee2a209eb50c1d
DeliaDumitrescu <deliadum123@gmail.com>
2022-01-23 11:26:48 +0200

    add graph route

--- plot.py
+++ plot.py
@@ -5,13 +5,39 @@ import time
 from flask import (
     Blueprint
 )
+import pylab as pl
+import numpy as np
+import pandas as pd
+
 
 bp = Blueprint('plot_api', __name__, url_prefix='/plot')
 
+
+def generate_plot(timestamps, values):
+    N = 7 * 24 # 7 days, 24 hours
+    np.random.seed(0)
+
+    day = np.random.randint(0, 7, N*2)
+    hour = np.random.randint(0, 24, N)
+    water_qty = np.random.randint(0, 200, N)
+
+    df = pd.DataFrame({"day": day, "hour": hour, "water_qty": water_qty})
+    df.drop_duplicates(subset=["day", "hour"], inplace=True)
+
+    df2 = df.pivot(columns="hour", index="day", values="water_qty")
+    df2.fillna(0, inplace=True)
+
+    day, hour = np.mgrid[:df2.shape[0]+1, :df2.shape[1]+1]
+    fig, ax = pl.subplots(figsize=(12, 4))
+    ax.set_aspect("equal")
+    pl.pcolormesh(hour, day, df2.values, cmap="Greens", edgecolor="w", vmin=-10, vmax=100)
+    pl.xlim(0, df2.shape[1])
+    fig.savefig('a.jpg')
+
+
+
 @bp.route('/')
 def plot():
-    # current_date = datetime.fromtimestamp(time.time())
-
     data = get_db().execute(
         'SELECT timestamp, value'
         ' FROM water'
@@ -20,10 +46,6 @@ def plot():
     timestamps = [x[0] for x in data]
     values = [x[1] for x in data]
 
-    
+    generate_plot(timestamps, values)
 
     return "SUCCESS", 200
-
-# print("current_date =", current_date)
-# print("type(current_date) =", type(current_date))
-
