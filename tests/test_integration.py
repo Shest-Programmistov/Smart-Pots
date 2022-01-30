707b08df2066d4c31b08aa6ec25a381244b1dff7
DeliaDumitrescu <deliadum123@gmail.com>
2022-01-30 19:40:17 +0200

    refactor integration test

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -34,15 +34,17 @@ def client(app):
     return app.test_client()
 
 
-def authorize(client):
-    client.post('/auth/register', data={'username': 'user', 'password': 'password'})
-    client.post('auth/login', data={'username': 'user', 'password': 'password'})
-
-
 @pytest.mark.integtest
 def test_app(app, client):
-    authorize(client)
     with app.app_context():
+        # Register
+        response = client.post('/auth/register', data={'username': 'user', 'password': 'password'})
+        assert response.status_code == 200
+
+        # Login
+        response = client.post('auth/login', data={'username': 'user', 'password': 'password'})
+        assert response.status_code == 200
+
         # Set plant characteristics
         response = client.post('/characteristics/set', data={'ideal_humidity': 70, 'ideal_temperature': 10}, follow_redirects=True)
         assert response.status_code == 200
@@ -85,3 +87,7 @@ def test_app(app, client):
         # Water status => should tell us not to water
         response = water.get_status()
         assert response['status'] == 'success' and response['data']['water'] == 0
+
+        # Logout
+        response = client.get('/auth/logout', data={'username': 'user', 'password': 'password'})
+        assert response.status_code == 200
